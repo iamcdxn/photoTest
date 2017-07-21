9a6a01a7c74358fc81f7e19dcd1953c92934df5f
CdxN <tiv0505@gmail.com>
2017-07-21 15:16:27 +0800

    Finished Test!

--- photoTestJames/photoTestJames/ViewController.swift
+++ photoTestJames/photoTestJames/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var imageView: UIImageView!
 
+    
     @IBOutlet var PickImageBtn: UIButton!
 
     override func viewDidLoad() {
@@ -29,8 +30,42 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
 
         self.scrollView.minimumZoomScale = 1.0
         self.scrollView.maximumZoomScale = 6.0
+        
+//        let tapGestureRecognizer = UITapGestureRecognizer( target:self, action: "loadImage:")
+//        tapGestureRecognizer.numberOfTapsRequired = 1
+//        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
+        
+    }
+    //recognizer: UITapGestureRecognizer
+    func loadImage () {
+     
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+//        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        } else{
+            print("Something went wrong")
+        }
+
+        self.dismiss(animated: true, completion: nil)
     }
 
+
+    @IBAction func addImage(_ sender: Any) {
+        loadImage()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
