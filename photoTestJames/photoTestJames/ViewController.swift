fb45f9af19cf78a9c471f6307ed56f3d3a23de60
CdxN <tiv0505@gmail.com>
2017-07-21 16:27:51 +0800

    Modified App Icon and AutoLayout

--- photoTestJames/photoTestJames/ViewController.swift
+++ photoTestJames/photoTestJames/ViewController.swift
@@ -12,45 +12,52 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var imageView: UIImageView!
-
-    @IBOutlet var PickImageBtn: UIButton!
+    @IBOutlet var pickImageBtn: UIButton!
+    var btnSubView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.PickImageBtn.titleLabel!.font = UIFont(name: "SF-UI-Text-Heavy", size: 30)
-    
-        PickImageBtn.layer.shadowColor = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
-        PickImageBtn.layer.shadowOpacity = 0.25
-        PickImageBtn.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-        PickImageBtn.layer.shadowRadius = 4.0
-        
-        PickImageBtn.layer.cornerRadius = 2.0
 
+        // Init ImageView
+        imageView.image = UIImage(named: "icon_photo")
+        imageView.contentMode = .center
+
+        // Setting BtnStyle
+        self.pickImageBtn.titleLabel!.font = UIFont(name: "SFUIText-Heavy", size: 20)
+        pickImageBtn.layer.shadowColor = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        pickImageBtn.layer.shadowOpacity = 0.25
+        pickImageBtn.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        pickImageBtn.layer.shadowRadius = 4.0
+        pickImageBtn.layer.cornerRadius = 2.0
+        
+        // Create btnSubView
+        btnSubView.frame = CGRect(x: 0.0, y: 0.0, width: pickImageBtn.frame.width * 2, height: pickImageBtn.frame.height / 2)
+        btnSubView.backgroundColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 0.05)
+        btnSubView.isUserInteractionEnabled = false
+        btnSubView.isExclusiveTouch = false
+
+        // Add SubView to Btn
+        pickImageBtn.addSubview(btnSubView)
+        pickImageBtn.clipsToBounds = true
+        
+        // Setting ScrollView ZoomScale
         self.scrollView.minimumZoomScale = 1.0
         self.scrollView.maximumZoomScale = 6.0
-        
-//        let tapGestureRecognizer = UITapGestureRecognizer( target:self, action: "loadImage:")
-//        tapGestureRecognizer.numberOfTapsRequired = 1
-//        imageView.addGestureRecognizer(tapGestureRecognizer)
-        
-        
     }
-    //recognizer: UITapGestureRecognizer
-    func loadImage () {
-     
+
+    // Load Image Function
+    func loadImage() {
+
         let imagePicker = UIImagePickerController()
+
         imagePicker.delegate = self
         imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
         
         self.present(imagePicker, animated: true, completion: nil)
-        
     }
     
+    // Set Picked Image
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
-//        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
         
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = image
@@ -58,20 +65,30 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
             print("Something went wrong")
         }
 
+        imageView.contentMode = .scaleAspectFit
         self.dismiss(animated: true, completion: nil)
     }
 
-
-    @IBAction func addImage(_ sender: Any) {
-        loadImage()
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Press Btn
+    @IBAction func loadImage(_ sender: Any) {
+    loadImage()
     }
 
+    // Setting Zoom
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
      return self.imageView
+    }
+    
+    // Print Font Name If Needed
+    func printFontNames() {
+        for family: String in UIFont.familyNames
+        {
+            print("\(family)")
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
+            }
+        }
     }
 }
 
